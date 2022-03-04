argilla-io/argilla

feat(#950): include search keywords as part of record results (#1201)
* chore: include search_keywords in client records

* chore: signatures

* feat: include search_records as part of client records

* fix: add highlight on dataset scan

* test: add missing tests

* test: estabilize tests

* Apply suggestions from code review

Co-authored-by: David Fidalgo <david@recogn.ai>

* test: try to fix push to hf hub

Co-authored-by: David Fidalgo <david@recogn.ai>

tests/functional_tests/test_log_for_token_classification.py
@@ -444,3 +444,28 @@ def test_log_record_that_makes_me_cry(mocked_client):
         },
         "annotated": {"mentions": []},
     }
+
+
+def test_search_keywords(mocked_client):
+    dataset = "test_search_keywords"
+    from datasets import load_dataset
+
+    dataset_ds = load_dataset("rubrix/gutenberg_spacy-ner_sm", split="train")
+    dataset_rb = rubrix.read_datasets(dataset_ds, task="TokenClassification")
+
+    rubrix.delete(dataset)
+    rubrix.log(name=dataset, records=dataset_rb)
+
+    df = rubrix.load(dataset, query="lis*")
+    assert not df.empty
+    assert "search_keywords" in df.columns
+    top_keywords = set(
+        [
+            keyword
+            for keywords in df.search_keywords.value_counts(sort=True, ascending=False)
+            .index[:3]
+            .tolist()
+            for keyword in keywords
+        ]
+    )
+    assert {"listened", "listen"} == top_keywords, top_keywords

Extralit/extralit

(cherry picked from commit 0678043e33344996d798c8cb5d80a176e57a7275)

tests/functional_tests/test_log_for_token_classification.py
@@ -446,3 +446,28 @@ def test_log_record_that_makes_me_cry(mocked_client):
         },
         "annotated": {"mentions": []},
     }
+
+
+def test_search_keywords(mocked_client):
+    dataset = "test_search_keywords"
+    from datasets import load_dataset
+
+    dataset_ds = load_dataset("rubrix/gutenberg_spacy-ner", split="train")
+    dataset_rb = rubrix.read_datasets(dataset_ds, task="TokenClassification")
+
+    rubrix.delete(dataset)
+    rubrix.log(name=dataset, records=dataset_rb)
+
+    df = rubrix.load(dataset, query="lis*")
+    assert not df.empty
+    assert "search_keywords" in df.columns
+    top_keywords = set(
+        [
+            keyword
+            for keywords in df.search_keywords.value_counts(sort=True, ascending=False)
+            .index[:3]
+            .tolist()
+            for keyword in keywords
+        ]
+    )
+    assert {"listened", "listen"} == top_keywords, top_keywords