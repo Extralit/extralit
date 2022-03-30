argilla-io/argilla

feat(#950): using record search_keywords for highlighting (#1235)
* feat(#950): using record search_keywords for highlighting

* feat(highlight): keywords for text-classification

* feat(highlight): keywords for text2text

* test: update snapshots

* fix: compute search keywords for old datasets, based on 'words' field

frontend/components/text-classifier/results/RecordExplain.vue
@@ -33,19 +33,18 @@
 <script>
 export default {
   props: {
-    queryText: {
-      type: String,
-      default: undefined,
+    record: {
+      type: Object,
+      required: true,
     },
     explain: {
       type: Array,
-    },
-    predicted: {
-      type: String,
-      default: undefined,
-    },
+    }
   },
   computed: {
+    predicted() {
+      return this.record.predicted;
+    },
     explainFormatted() {
       // TODO ALLOW FOR MULTI LABEL
       return this.explain.map((token) => {
@@ -58,8 +57,8 @@ export default {
           percent = Math.round(Math.log10(percent) ** p * s);
         }
         return {
-          text: this.queryText
-            ? this.$highlightSearch(this.queryText, token.token)
+          text: this.record.search_keywords
+            ? this.$highlightKeyword(token.token, this.record.search_keywords)
             : token.token,
           percent: percent.toString(),
           grad,

Extralit/extralit

(cherry picked from commit 9e11933a50cf5f613f8441aaa3c3defb938870b9)
fix(#950):  improve highlight for multi terms searches (#1278)
* fix(hightlight): merge adjacent terms
* refactor: apply multi-keyword search in text
* feat: merge highlighted phrases
* feat: parse keywords as entire words
* fix(highlight): include hightlight info at visual token level
* chore: lint
* fix: escape html before apply highligh span
* fix calc whitespace for highlighted entities
* fix: parse middle highlighted tokens
* refactor: highligh on inputs
* test: update tests
Co-authored-by: LeireA <leire@recogn.ai>
(cherry picked from commit 3a3233460d7ee5e9dad0abccb202fceb5ea2ab1f)
chore(#1235): fix highlight function name in explain (#1316)
Closes #1315
(cherry picked from commit 41b3321ca31d513e46e292550ae16ac3f7a1f414)

frontend/components/text-classifier/results/RecordExplain.vue
@@ -33,19 +33,18 @@
 <script>
 export default {
   props: {
-    queryText: {
-      type: String,
-      default: undefined,
+    record: {
+      type: Object,
+      required: true,
     },
     explain: {
       type: Array,
-    },
-    predicted: {
-      type: String,
-      default: undefined,
-    },
+    }
   },
   computed: {
+    predicted() {
+      return this.record.predicted;
+    },
     explainFormatted() {
       // TODO ALLOW FOR MULTI LABEL
       return this.explain.map((token) => {
@@ -58,8 +57,8 @@ export default {
           percent = Math.round(Math.log10(percent) ** p * s);
         }
         return {
-          text: this.queryText
-            ? this.$highlightSearch(this.queryText, token.token)
+          text: this.record.search_keywords
+            ? this.$highlightKeywords(token.token, this.record.search_keywords)
             : token.token,
           percent: percent.toString(),
           grad,