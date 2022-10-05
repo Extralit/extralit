argilla-io/argilla

feat(Datasets): delete records by query (#1721)
Closes #1714

src/rubrix/client/sdk/client.py
@@ -121,6 +121,17 @@ class Client(_ClientCommonDefaults, _Client):
         return build_raw_response(response).parsed
 
     @with_httpx_error_handler
+    def delete(self, path: str, *args, **kwargs):
+        path = self._normalize_path(path)
+        response = self.__httpx__.delete(
+            url=path,
+            headers=self.get_headers(),
+            *args,
+            **kwargs,
+        )
+        return build_raw_response(response).parsed
+
+    @with_httpx_error_handler
     def stream(self, path: str, *args, **kwargs):
         return self.__httpx__.stream(
             url=path,

Extralit/extralit

(cherry picked from commit be4e92cf1366ac2d778fd5f68ee21cf00c8f8705)
fix: delete records feature (#1737)
Some problems detected:
Change from httpx.delete to httpx.request with method="DELETE", since the delete method does not accept body param (data or json)
Fix response data when no records match for deletion

src/rubrix/client/sdk/client.py
@@ -121,6 +121,18 @@ class Client(_ClientCommonDefaults, _Client):
         return build_raw_response(response).parsed
 
     @with_httpx_error_handler
+    def delete(self, path: str, *args, **kwargs):
+        path = self._normalize_path(path)
+        response = self.__httpx__.request(
+            method="DELETE",
+            url=path,
+            headers=self.get_headers(),
+            *args,
+            **kwargs,
+        )
+        return build_raw_response(response).parsed
+
+    @with_httpx_error_handler
     def stream(self, path: str, *args, **kwargs):
         return self.__httpx__.stream(
             url=path,