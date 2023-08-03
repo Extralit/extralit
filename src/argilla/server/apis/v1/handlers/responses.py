Extralit/extralit

commit edad52a1cf8ec24895b6d523e610f6c98e3fe8d9

feat: create user with workspaces (#3462)
# Description

This PR updates the API and the Python client so a list of workspaces
names to which the user should be linked to can be provided. This is
useful when creating a new user, as just one request will be needed,
instead of the current behaviour, which requires one to create the user
and another one to add the user to the workspace.

In addition, the code blocks in which a commit or a rollback has to be
done depending on wether an exception is raised have been updated to use
the async context manager `async with session.begin_nested():` which
automatically will commit if the block is executed without any problem
or rollback if an exception is raised.

**Type of change**

- [x] New feature (non-breaking change which adds functionality)
- [x] Improvement (change adding some improvement to an existing
functionality)

**How Has This Been Tested**

I've been able to create a user with workspaces in a local development
environment. Also, the unit tests regarding the user creation have been
updated.

**Checklist**

- [ ] I added relevant documentation
- [x] follows the style guidelines of this project
- [x] I did a self-review of my code
- [x] I made corresponding changes to the documentation
- [x] My changes generate no new warnings
- [x] I have added tests that prove my fix is effective or that my
feature works
- [ ] I filled out [the contributor form](https://tally.so/r/n9XrxK)
(see text above)
- [x] I have added relevant notes to the CHANGELOG.md file (See
https://keepachangelog.com/)

src/argilla/server/contexts/datasets.py
@@ -111,25 +111,17 @@ async def publish_dataset(db: "AsyncSession", search_engine: SearchEngine, datas
     if await _count_questions_by_dataset_id(db, dataset.id) == 0:
         raise ValueError("Dataset cannot be published without questions")
 
-    try:
+    async with db.begin_nested():
         dataset = await dataset.update(db, status=DatasetStatus.ready, autocommit=False)
         await search_engine.create_index(dataset)
-        await db.commit()
-    except:
-        await db.rollback()
-        raise
 
     return dataset
 
 
 async def delete_dataset(db: "AsyncSession", search_engine: SearchEngine, dataset: Dataset) -> Dataset:
     dataset = await dataset.delete(db)
-    try:
+    async with db.begin_nested():
         await search_engine.delete_index(dataset)
-        await db.commit()
-    except:
-        await db.rollback()
-        raise
     return dataset
 
 
@@ -346,16 +338,12 @@ async def create_records(
 
         records.append(record)
 
-    try:
+    async with db.begin_nested():
         db.add_all(records)
         await db.flush(records)
         for record in records:
             await record.awaitable_attrs.responses
         await search_engine.add_records(dataset, records)
-        await db.commit()
-    except:
-        await db.rollback()
-        raise
 
 
 async def get_response_by_id(db: "AsyncSession", response_id: UUID) -> Union[Response, None]:
@@ -403,13 +391,9 @@ async def create_response(
         autocommit=False,
     )
 
-    try:
+    async with db.begin_nested():
         await db.flush([response])
         await search_engine.update_record_response(response)
-        await db.commit()
-    except Exception:
-        await db.rollback()
-        raise
 
     return response
 
@@ -423,24 +407,16 @@ async def update_response(
         db, values=jsonable_encoder(response_update.values), status=response_update.status, autocommit=False
     )
 
-    try:
+    async with db.begin_nested():
         await search_engine.update_record_response(response)
-        await db.commit()
-    except Exception:
-        await db.rollback()
-        raise
 
     return response
 
 
 async def delete_response(db: "AsyncSession", search_engine: SearchEngine, response: Response) -> Response:
     response = await response.delete(db)
-    try:
+    async with db.begin_nested():
         await search_engine.delete_record_response(response)
-        await db.commit()
-    except Exception:
-        await db.rollback()
-        raise
     return response
 
 

src/argilla/client/sdk/users/models.py
@@ -32,6 +32,7 @@ class UserCreateModel(BaseModel):
     username: str = Field(min_length=1, regex=r"^(?!-|_)[a-z0-9-_]+$")
     role: UserRole = UserRole.annotator
     password: str = Field(min_length=8, max_length=100)
+    workspaces: Optional[List[str]] = None
 
     # TODO(alvarobartt): confirm with @frascuchon
     # disabled: Optional[bool] = None

src/argilla/client/sdk/users/api.py
@@ -99,6 +99,7 @@ def create_user(
     password: str,
     last_name: Optional[str] = None,
     role: UserRole = UserRole.annotator,
+    workspaces: Optional[List[str]] = None,
 ) -> Response[Union[UserModel, ErrorMessage, HTTPValidationError]]:
     """Sends a POST request to `/api/users` endpoint to create a new user.
 
@@ -111,6 +112,7 @@ def create_user(
             ^(?!-|_)[a-z0-9-_]+$.
         role: the role of the user. Available roles are: `admin`, and `annotator`.
         password: the password of the user. Must be a string between 8 and 100 characters.
+        workspaces: a list of workspace names to which the user will be linked to.
 
     Returns:
         A `Response` object containing a `parsed` attribute with the parsed response if
@@ -124,6 +126,7 @@ def create_user(
         username=username,
         role=role,
         password=password,
+        workspaces=workspaces,
     )
 
     response = client.post(

src/argilla/client/users.py
@@ -18,9 +18,14 @@ from typing import TYPE_CHECKING, Iterator, List, Optional, Union
 from uuid import UUID
 
 from argilla.client import active_client
-from argilla.client.sdk.commons.errors import AlreadyExistsApiError, BaseClientError, NotFoundApiError
+from argilla.client.sdk.commons.errors import (
+    AlreadyExistsApiError,
+    BaseClientError,
+    NotFoundApiError,
+    ValidationApiError,
+)
 from argilla.client.sdk.users import api as users_api
-from argilla.client.sdk.users.models import UserCreateModel, UserModel, UserRole
+from argilla.client.sdk.users.models import UserModel, UserRole
 from argilla.client.sdk.v1.users import api as users_api_v1
 from argilla.client.sdk.v1.workspaces import api as workspaces_api_v1
 from argilla.client.utils import allowed_for_roles
@@ -176,6 +181,7 @@ class User:
         first_name: Optional[str] = None,
         last_name: Optional[str] = None,
         role: Optional[UserRole] = None,
+        workspaces: Optional[List[str]] = None,
     ) -> "User":
         """Creates a new user in Argilla.
 
@@ -185,6 +191,7 @@ class User:
             first_name: the first name of the user to be created. Defaults to None.
             last_name: the last name of the user to be created. Defaults to None.
             role: the role of the user to be created. Defaults to None.
+            workspaces: a list of workspace names the user to be created is linked to. Defaults to None.
 
         Returns:
             A new `User` instance.
@@ -210,19 +217,24 @@ class User:
         try:
             user = users_api.create_user(
                 client,
-                **UserCreateModel(
-                    username=username,
-                    password=password,
-                    first_name=first_name,
-                    last_name=last_name,
-                    role=role,
-                ).dict(),
+                username=username,
+                password=password,
+                first_name=first_name,
+                last_name=last_name,
+                role=role,
+                workspaces=workspaces,
             ).parsed
             return cls.__new_instance(client, user)
         except AlreadyExistsApiError as e:
             raise ValueError(
                 f"User with username=`{username}` already exists in Argilla, so please"
                 " make sure that the name you provided is a unique one."
+            ) from e
+        except ValidationApiError as e:
+            response = e.ctx["response"]
+            raise ValueError(
+                f"User with username=`{username}` cannot be created in Argilla, as"
+                f" the provided data is not valid. Please, check the following error: {response}"
             ) from e
         except BaseClientError as e:
             raise RuntimeError(f"Error while creating user with username=`{username}` in Argilla.") from e

src/argilla/server/apis/v0/handlers/users.py
@@ -16,7 +16,7 @@
 from typing import List
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, Request, Security
+from fastapi import APIRouter, Depends, HTTPException, Request, Security, status
 from pydantic import parse_obj_as
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -95,9 +95,12 @@ async def create_user(
     if user is not None:
         raise EntityAlreadyExistsError(name=user_create.username, type=User)
 
-    user = await accounts.create_user(db, user_create)
-    await user.awaitable_attrs.workspaces
+    try:
+        user = await accounts.create_user(db, user_create)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(e))
 
+    await user.awaitable_attrs.workspaces
     return User.from_orm(user)
 
 

src/argilla/server/contexts/accounts.py
@@ -109,14 +109,30 @@ async def list_users(db: "AsyncSession") -> List[User]:
 
 
 async def create_user(db: "AsyncSession", user_create: UserCreate) -> User:
-    return await User.create(
-        db,
-        first_name=user_create.first_name,
-        last_name=user_create.last_name,
-        username=user_create.username,
-        role=user_create.role,
-        password_hash=hash_password(user_create.password),
-    )
+    async with db.begin_nested():
+        user = await User.create(
+            db,
+            first_name=user_create.first_name,
+            last_name=user_create.last_name,
+            username=user_create.username,
+            role=user_create.role,
+            password_hash=hash_password(user_create.password),
+            autocommit=False,
+        )
+
+        if user_create.workspaces:
+            for workspace_name in user_create.workspaces:
+                workspace = await get_workspace_by_name(db, workspace_name)
+                if not workspace:
+                    raise ValueError(f"Workspace '{workspace_name}' does not exist")
+                await WorkspaceUser.create(
+                    db,
+                    workspace_id=workspace.id,
+                    user_id=user.id,
+                    autocommit=False,
+                )
+
+    return user
 
 
 async def delete_user(db: "AsyncSession", user: User) -> User:

src/argilla/server/security/model.py
@@ -57,6 +57,7 @@ class UserCreate(BaseModel):
     username: constr(regex=USER_USERNAME_REGEX, min_length=1)
     role: Optional[UserRole]
     password: constr(min_length=USER_PASSWORD_MIN_LENGTH, max_length=USER_PASSWORD_MAX_LENGTH)
+    workspaces: Optional[List[str]]
 
 
 class UserGetter(GetterDict):

tests/client/sdk/users/test_api.py
@@ -25,14 +25,12 @@ from argilla.client.sdk.commons.errors import (
     ForbiddenApiError,
     NotFoundApiError,
     UnauthorizedApiError,
+    ValidationApiError,
 )
-from argilla.client.sdk.users.api import (
-    create_user,
-    delete_user,
-    list_users,
-    whoami,
-)
+from argilla.client.sdk.users.api import create_user, delete_user, list_users, whoami
 from argilla.client.sdk.users.models import UserModel
+
+from tests.factories import WorkspaceFactory
 
 if TYPE_CHECKING:
     from argilla.server.models import User as ServerUser
@@ -66,15 +64,28 @@ def test_list_users(owner: "ServerUser") -> None:
 
 
 @pytest.mark.parametrize("role", ["annotator", "admin", "owner"])
-def test_create_user(owner: "ServerUser", role: str) -> None:
+@pytest.mark.asyncio
+async def test_create_user(owner: "ServerUser", role: str) -> None:
+    await WorkspaceFactory.create(name="workspace_1")
+    await WorkspaceFactory.create(name="workspace_2")
+
     httpx_client = ArgillaSingleton.init(api_key=owner.api_key).http_client.httpx
 
     response = create_user(
-        client=httpx_client, first_name="user", username="user_1", password="user_password", role=role
+        client=httpx_client,
+        first_name="user",
+        username="user_1",
+        password="user_password",
+        role=role,
+        workspaces=["workspace_1", "workspace_2"],
     )
+
     assert response.status_code == 200
     assert isinstance(response.parsed, UserModel)
+    assert response.parsed.full_name == "user"
+    assert response.parsed.username == "user_1"
     assert response.parsed.role == role
+    assert response.parsed.workspaces == ["workspace_1", "workspace_2"]
 
 
 def test_create_user_errors(owner: "ServerUser", annotator: "ServerUser") -> None:
@@ -92,6 +103,16 @@ def test_create_user_errors(owner: "ServerUser", annotator: "ServerUser") -> Non
             username=annotator.username,
             password="user_password",
             role="annotator",
+        )
+
+    with pytest.raises(ValidationApiError):
+        create_user(
+            client=httpx_client,
+            first_name="another-user",
+            username="another-user",
+            password="user_password",
+            role="annotator",
+            workspaces=["i do not exist"],
         )
 
 

commit 2a6b25a53e99fa9a4ae26145846b29541bd3b404

fix: value of responses are not updated (#3500)
# Description

This PR fixes the issue (reported by @damianpumar), where the values of
an already created response for a given record were not updated, even
the server returned a `200`. The bug was introduced in #3462, where the
`try: ... db.commit() except Exception: ... db.rollback()` where
replaced with `async with db.begin_nested()` block.

**Type of change**

- [x] Bug fix (non-breaking change which fixes an issue)

**How Has This Been Tested**

The Docker image of this branch has been deployed in `dev.argilla.io`,
where I was able to update the values of a response, refresh the page
and see the updated values.

**Checklist**

- [ ] I added relevant documentation
- [x] follows the style guidelines of this project
- [x] I did a self-review of my code
- [x] I made corresponding changes to the documentation
- [x] My changes generate no new warnings
- [ ] I have added tests that prove my fix is effective or that my
feature works
- [ ] I filled out [the contributor form](https://tally.so/r/n9XrxK)
(see text above)
- [x] I have added relevant notes to the CHANGELOG.md file (See
https://keepachangelog.com/)

src/argilla/server/apis/v1/handlers/responses.py
@@ -19,9 +19,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from argilla.server.contexts import datasets
 from argilla.server.database import get_async_db
-from argilla.server.models import User
+from argilla.server.models import Response, User
 from argilla.server.policies import ResponsePolicyV1, authorize
-from argilla.server.schemas.v1.responses import Response, ResponseUpdate
+from argilla.server.schemas.v1.responses import Response as ResponseSchema
+from argilla.server.schemas.v1.responses import ResponseUpdate
 from argilla.server.search_engine import SearchEngine, get_search_engine
 from argilla.server.security import auth
 
@@ -38,7 +39,7 @@ async def _get_response(db: AsyncSession, response_id: UUID) -> Response:
     return response
 
 
-@router.put("/responses/{response_id}", response_model=Response)
+@router.put("/responses/{response_id}", response_model=ResponseSchema)
 async def update_response(
     *,
     db: AsyncSession = Depends(get_async_db),
@@ -59,7 +60,7 @@ async def update_response(
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(err))
 
 
-@router.delete("/responses/{response_id}", response_model=Response)
+@router.delete("/responses/{response_id}", response_model=ResponseSchema)
 async def delete_response(
     *,
     db: AsyncSession = Depends(get_async_db),

src/argilla/server/contexts/datasets.py
@@ -121,8 +121,8 @@ async def publish_dataset(db: "AsyncSession", search_engine: SearchEngine, datas
 
 
 async def delete_dataset(db: "AsyncSession", search_engine: SearchEngine, dataset: Dataset) -> Dataset:
-    dataset = await dataset.delete(db)
     async with db.begin_nested():
+        dataset = await dataset.delete(db, autocommit=False)
         await search_engine.delete_index(dataset)
     return dataset
 
@@ -411,16 +411,15 @@ async def create_response(
 ) -> Response:
     validate_response_values(record.dataset, values=response_create.values, status=response_create.status)
 
-    response = await Response.create(
-        db,
-        values=jsonable_encoder(response_create.values),
-        status=response_create.status,
-        record_id=record.id,
-        user_id=user.id,
-        autocommit=False,
-    )
-
     async with db.begin_nested():
+        response = await Response.create(
+            db,
+            values=jsonable_encoder(response_create.values),
+            status=response_create.status,
+            record_id=record.id,
+            user_id=user.id,
+            autocommit=False,
+        )
         await db.flush([response])
         await search_engine.update_record_response(response)
 
@@ -432,19 +431,18 @@ async def update_response(
 ):
     validate_response_values(response.record.dataset, values=response_update.values, status=response_update.status)
 
-    response = await response.update(
-        db, values=jsonable_encoder(response_update.values), status=response_update.status, autocommit=False
-    )
-
     async with db.begin_nested():
+        response = await response.update(
+            db, values=jsonable_encoder(response_update.values), status=response_update.status, autocommit=False
+        )
         await search_engine.update_record_response(response)
 
     return response
 
 
 async def delete_response(db: "AsyncSession", search_engine: SearchEngine, response: Response) -> Response:
-    response = await response.delete(db)
     async with db.begin_nested():
+        response = await response.delete(db, autocommit=False)
         await search_engine.delete_record_response(response)
     return response
 

src/argilla/server/models/models.py
@@ -82,7 +82,7 @@ ResponseStatusEnum = SAEnum(ResponseStatus, name="response_status_enum")
 class Response(DatabaseModel):
     __tablename__ = "responses"
 
-    values: Mapped[Optional[dict]] = mapped_column(JSON)
+    values: Mapped[Optional[dict]] = mapped_column(MutableDict.as_mutable(JSON))
     status: Mapped[ResponseStatus] = mapped_column(ResponseStatusEnum, default=ResponseStatus.submitted, index=True)
     record_id: Mapped[UUID] = mapped_column(ForeignKey("records.id", ondelete="CASCADE"), index=True)
     user_id: Mapped[Optional[UUID]] = mapped_column(ForeignKey("users.id", ondelete="SET NULL"), index=True)

tests/unit/server/api/v1/test_responses.py
@@ -20,7 +20,6 @@ import pytest
 from argilla._constants import API_KEY_HEADER_NAME
 from argilla.server.models import DatasetStatus, Response, ResponseStatus, UserRole
 from argilla.server.search_engine import SearchEngine
-from fastapi.testclient import TestClient
 from sqlalchemy import func, select
 
 from tests.factories import (
@@ -35,6 +34,7 @@ from tests.factories import (
 )
 
 if TYPE_CHECKING:
+    from httpx import AsyncClient
     from sqlalchemy.ext.asyncio import AsyncSession
 
 