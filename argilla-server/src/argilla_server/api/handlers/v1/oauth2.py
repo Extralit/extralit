5177795c730618f9228ec7d7af7e56e3e4a0e934
JonnyTran <nhat.c.tran@gmail.com>
2025-06-29 12:32:51 -0700

    fix: ensure workspace user deletion is safe and refresh oauth user after token retrieval
    - Added a check to ensure `ws_user` exists before attempting to delete it, preventing potential errors.
    - Included a call to refresh the `oauth_user` after the access token is generated to ensure the latest state is reflected.

--- argilla-server/src/argilla_server/api/handlers/v1/oauth2.py
+++ argilla-server/src/argilla_server/api/handlers/v1/oauth2.py
@@ -1,16 +1,16 @@
-#  Copyright 2021-present, the Recognai S.L. team.
+# Copyright 2024-present, Extralit Labs, Inc.
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from fastapi import APIRouter, Depends, Request, Path
 from fastapi.responses import RedirectResponse
@@ -89,7 +89,8 @@ async def get_access_token(
         for workspace in oauth_workspaces:
             if workspace.name not in available_workspaces:
                 ws_user = await WorkspaceUser.get_by(db, workspace_id=workspace.id, user_id=oauth_user.id)
-                await ws_user.delete(db, autocommit=False)
+                if ws_user:
+                    await ws_user.delete(db, autocommit=False)
         # Sync added workspaces
         for workspace_name in available_workspaces:
             if workspace_name in [ws.name for ws in oauth_workspaces]:
@@ -107,5 +108,6 @@ async def get_access_token(
                     autocommit=False,
                 )
         await db.commit()
+        await db.refresh(oauth_user)
 
     return Token(access_token=accounts.generate_user_token(oauth_user))
