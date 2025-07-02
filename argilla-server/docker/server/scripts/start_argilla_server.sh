96d8ac5b02e138ec88ef6e1930dbfb57af5a205b
JonnyTran <nhat.c.tran@gmail.com>
2025-07-02 16:27:52 -0700

    chore: update Copilot setup to conditionally run frontend in development mode

--- argilla-server/docker/server/scripts/start_argilla_server.sh
+++ argilla-server/docker/server/scripts/start_argilla_server.sh
@@ -41,14 +41,6 @@ if [ "$REINDEX_DATASETS" == "true" ] || [ "$REINDEX_DATASETS" == "1" ] || [ "$in
   python -m argilla_server search-engine reindex
 fi
 
-# Run argilla-server (See https://www.uvicorn.org/settings/#settings)
-#
-# From uvicorn docs:
-#   You can also configure Uvicorn using environment variables
-#   with the prefix UVICORN_. For example, in case you want to
-#   run the app on port 5000, just set the environment variable
-#   UVICORN_PORT to 5000.
-
 if [ "$ENV" = "dev" ]; then
   echo 'Running in development mode'
   uvicorn $UVICORN_APP --host 0.0.0.0 --port $UVICORN_PORT --workers 4 --timeout-keep-alive 75 --reload
