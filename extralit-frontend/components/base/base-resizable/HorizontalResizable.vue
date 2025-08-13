aa2e301596bdd601bf2669ad632db1f2b101e43d
JonnyTran <nhat.c.tran@gmail.com>
2025-08-13 13:45:14 -0700

    fix deps

--- extralit-frontend/components/base/base-resizable/HorizontalResizable.vue
+++ extralit-frontend/components/base/base-resizable/HorizontalResizable.vue
@@ -125,7 +125,7 @@ export default {
     }
   },
   destroyed() {
-    this.resizer.removeEventListener(EVENT.MOUSE_DOWN, this.mouseDownHandler);
+    this.resizer?.removeEventListener(EVENT.MOUSE_DOWN, this.mouseDownHandler);
   },
   methods: {
     savePosition() {},
