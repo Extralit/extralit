481f4538520b4517a0d6ac78a117b089d12f8783
JonnyTran <nhat.c.tran@gmail.com>
2025-08-14 15:06:08 -0700

    Refactor imports in __init__.py files and update linting rules in pyproject.toml to include I001 for unused imports.

--- extralit/src/extralit/__init__.py
+++ extralit/src/extralit/__init__.py
@@ -16,11 +16,11 @@ from extralit._version import __version__
 from extralit.client import *
 from extralit.datasets import *
 from extralit.documents import *
-from extralit.records import *
-from extralit.responses import *
+from extralit.workspaces import *
+from extralit.users import *
 from extralit.settings import *
 from extralit.suggestions import *
-from extralit.users import *
+from extralit.responses import *
+from extralit.records import *
 from extralit.vectors import *
 from extralit.webhooks import *
-from extralit.workspaces import *
