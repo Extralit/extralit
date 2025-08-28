76b689856906a3e482d28b5ff6210cf492ab3d91
JTran-IDM <nhat.tran@mavs.uta.edu>
2025-08-28 16:15:49 -0700

    fix DatasetMapping

--- extralit/src/extralit/settings/_mapping.py
+++ extralit/src/extralit/settings/_mapping.py
@@ -14,7 +14,7 @@
 
 from abc import ABC, abstractmethod
 from collections.abc import Sequence
-from typing import Any, Union
+from typing import Any, Optional, Union
 
 from extralit._models._settings._mapping import DatasetMappingModel
 
@@ -81,8 +81,12 @@ class DatasetMapping(DatasetMappingBase):
         return self._model.suggestions or []
 
     @property
-    def external_id(self) -> str:
-        return self._model.external_id
+    def source_id(self) -> Optional[str]:
+        return self._model.source_id
+
+    @property
+    def target_id(self) -> Optional[str]:
+        return self._model.target_id
 
     def __eq__(self, other):
         """Compare DatasetMapping objects for equality."""
