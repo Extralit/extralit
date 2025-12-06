a191ef9f74bb956469921ddcea3ec817782a060a
JonnyTran <nhat.c.tran@gmail.com>
2025-12-05 23:26:29 -0800

    refactor: remove unnecessary emits from RenderTable and BaseSimpleTable
    - Eliminated redundant event emissions for table updates and validation in RenderTable.
    - Cleaned up event handling in BaseSimpleTable to streamline communication with RenderTable.

--- extralit-frontend/components/base/base-render-table/RenderTable.vue
+++ extralit-frontend/components/base/base-render-table/RenderTable.vue
@@ -389,7 +389,6 @@ export default {
       }
 
       this.tableJSON.data = this.tabulator.getData().map(({ _id, ...rest }) => rest);
-      this.$emit("update-table-json");
     },
     isIndexRefColumn(field: string) {
       return this.indexColumns?.includes(field) || this.refColumns?.includes(field);
@@ -461,7 +460,6 @@ export default {
       if (options?.saveData == true) {
         this.updateTableJsonData();
       }
-      this.$emit("validate-table", isValid);
       return isValid;
     },
     toggleShowRefColumns() {
@@ -579,7 +577,6 @@ export default {
       }
 
       this.updateTableJsonData(false, false, true, newFieldName, oldFieldName);
-      this.$emit("column-title-changed", column);
       // this.tabulator?.setColumns(this.columnsConfig);
     },
     clearTable() {
