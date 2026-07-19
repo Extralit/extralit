1d45e51d02be902ecda0a1fa227fdb7ff4795a82
JonnyTran <nhat.c.tran@gmail.com>
2026-07-19 01:12:11 -0700

    feat(v2-ui): integrate schemas list into app shell; hydrate workspaces on direct load

--- extralit-frontend/pages/schemas/index.vue
+++ extralit-frontend/pages/schemas/index.vue
@@ -1,68 +1,100 @@
 <template>
-  <div class="schemas-page">
-    <h1 class="schemas-page__title" v-text="$t('schemas.title')" />
+  <InternalPage>
+    <template #header>
+      <AppHeader :breadcrumbs="breadcrumbs" />
+    </template>
+    <template #page-content>
+      <div class="schemas-page">
+        <h1 class="schemas-page__title" v-text="$t('schemas.title')" />
 
-    <p v-if="!selectedWorkspace" class="schemas-page__empty" v-text="$t('schemas.noWorkspace')" />
-    <BaseLoading v-else-if="isLoading" />
-    <p v-else-if="loadFailed" class="schemas-page__empty" v-text="$t('schemas.loadError')" />
-    <p v-else-if="!schemas.length" class="schemas-page__empty" v-text="$t('schemas.empty')" />
+        <V2Empty v-if="!selectedWorkspace" :message="$t('schemas.noWorkspace')" />
+        <BaseLoading v-else-if="isLoading" />
+        <V2Empty v-else-if="loadFailed" :message="$t('schemas.loadError')" />
+        <V2Empty v-else-if="!schemas.length" :message="$t('schemas.empty')" />
 
-    <table v-else class="schemas-page__table">
-      <thead>
-        <tr>
-          <th v-text="$t('schemas.name')" />
-          <th v-text="$t('schemas.status')" />
-          <th v-text="$t('schemas.updatedAt')" />
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="schema in schemas" :key="schema.id">
-          <td>
-            <NuxtLink :to="`/schemas/${schema.id}`">{{ schema.name }}</NuxtLink>
-          </td>
-          <td>{{ schema.status }}</td>
-          <td>{{ schema.updatedAt }}</td>
-          <td>
-            <NuxtLink :to="`/schemas/${schema.id}/settings`">{{ $t("schemas.settings") }}</NuxtLink>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+        <table v-else class="schemas-page__table">
+          <thead>
+            <tr>
+              <th v-text="$t('schemas.name')" />
+              <th v-text="$t('schemas.status')" />
+              <th v-text="$t('schemas.updatedAt')" />
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="schema in schemas" :key="schema.id">
+              <td>
+                <NuxtLink class="schemas-page__link" :to="`/schemas/${schema.id}`">{{ schema.name }}</NuxtLink>
+              </td>
+              <td><V2StatusBadge :status="schema.status" /></td>
+              <td>{{ schema.updatedAt }}</td>
+              <td>
+                <NuxtLink class="schemas-page__link" :to="`/schemas/${schema.id}/settings`">
+                  {{ $t("schemas.settings") }}
+                </NuxtLink>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </template>
+  </InternalPage>
 </template>
 
 <script lang="ts">
+import { computed, onBeforeMount } from "vue";
+import InternalPage from "@/layouts/InternalPage.vue";
 import { useSchemasViewModel } from "./useSchemasViewModel";
+import { useEnsureWorkspaces } from "~/composables/useEnsureWorkspaces";
+import { useV2Breadcrumbs } from "~/composables/useV2Breadcrumbs";
 
 export default {
+  components: { InternalPage },
   setup() {
-    return useSchemasViewModel();
+    const viewModel = useSchemasViewModel();
+    const { ensureWorkspaces } = useEnsureWorkspaces();
+    const { schemasBreadcrumbs } = useV2Breadcrumbs();
+    const breadcrumbs = computed(() => schemasBreadcrumbs());
+
+    onBeforeMount(async () => {
+      await ensureWorkspaces();
+      await viewModel.loadSchemas();
+    });
+
+    return { ...viewModel, breadcrumbs };
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .schemas-page {
-  padding: $base-space * 3;
+  padding: $base-space * 3 0;
 
   &__title {
     margin: 0 0 $base-space * 2;
+    @include font-size(24px);
   }
 
-  &__empty {
-    color: var(--fg-tertiary);
+  &__link {
+    color: var(--fg-cuaternary);
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
   }
 
   &__table {
     width: 100%;
     border-collapse: collapse;
-
     th,
     td {
       text-align: left;
       padding: $base-space;
       border-bottom: 1px solid var(--bg-opacity-10);
+    }
+    th {
+      color: var(--fg-tertiary);
+      @include font-size(12px);
     }
   }
 }
